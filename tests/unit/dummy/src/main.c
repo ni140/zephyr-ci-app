f6ee0f0a90248872f92bda473c886adaca2d4fbb
Maciej <m.dobrynczuk@gmail.com>
2025-01-18 10:31:58 +0100

    add UTs

---
+++ tests/unit/dummy/src/main.c
@@ -0,0 +1,19 @@
+#include <zephyr/kernel.h>
+#include <zephyr/ztest.h>
+
+ZTEST(template, test_template) {
+  zassert_true(true, "Some test failed");
+  printk("test finished!\n");
+}
+
+ZTEST(template, test_assert) {
+  zassert_true(1, "1 was false");
+  printk("test finished!\n");
+}
+
+ZTEST(template, test_assert2) {
+  zassert_true(1, "1 was false");
+  printk("test finished!\n");
+}
+
+ZTEST_SUITE(template, NULL, NULL, NULL, NULL, NULL);
